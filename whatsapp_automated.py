52df39eb7d1b19ff4fb8209e2d990ec813d8d33e
Shivani Agarwal <77854814+shivani-1198@users.noreply.github.com>
2024-04-22 16:21:03 -0400

    Add files via upload

---
+++ whatsapp_automated.py
@@ -0,0 +1,5 @@
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+18374228737","*HAPPY BIRTHDAY* \n Have a great day ...!!!", 16, 16)
+
+#pywhatkit.sendwhatmsg("+1phonenumber","message", hour of the time to send, minute of the time to send)
